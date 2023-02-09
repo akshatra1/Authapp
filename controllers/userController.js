7de658e3ed9599505813aa99aeea340be04fb383
akshatra <akshatrauday@gmail.com>
2023-02-09 20:54:31 +0530

    generate jwt token for the registration and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,9 +21,19 @@ class UserController {
               tc: tc,
             });
             await doc.save();
-            res
-              .status(201)
-              .send({ status: "Success", message: "registration success" });
+            const saved_user = await userModel.findOne({ email: email });
+            //Generate JWT Token
+            const token = jwt.sign(
+              { userID: saved_user._id },
+              process.env.JWT_SECRET_KEY,
+              { expiresIn: "5d" }
+            );
+
+            res.status(201).send({
+              status: "Success",
+              message: "registration success",
+              token: token,
+            });
           } catch (error) {
             console.log(error);
             res.send({ status: "failed", message: "unable to register" });
@@ -36,6 +46,46 @@ class UserController {
       }
     }
   };
-}
 
+  static userLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (email && password) {
+        const user = await userModel.findOne({ email: email });
+        if (user != null) {
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (user.email === email && isMatch) {
+            // generate JWT Token
+            const token = jwt.sign(
+              { userID: user._id },
+              process.env.JWT_SECRET_KEY,
+              { expiresIn: "5d" }
+            );
+
+            res.send({
+              status: "sucess",
+              message: "Login Success",
+              token: token,
+            });
+          } else {
+            res.send({
+              status: "failed",
+              message: " email or password is not valid",
+            });
+          }
+        } else {
+          res.send({
+            status: "failed",
+            message: "you are not registered user",
+          });
+        }
+      } else {
+        res.send({ status: "failed", message: "all field are required" });
+      }
+    } catch (error) {
+      console.log(error);
+      res.send({ status: "failed", message: "Unable to Login" });
+    }
+  };
+}
 export default UserController;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,8 @@ import UserController from "../controllers/userController.js";
 
 //user registration
 router.post("/register", UserController.userRegistration);
+router.post("/login", UserController.userLogin);
+
+//
 
 export default router;
